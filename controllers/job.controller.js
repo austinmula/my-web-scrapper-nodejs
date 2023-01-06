14101f8db4980b1c7d4eaac99d6444d226fe7e18
cymula <amula@cytonn.com>
2023-01-07 02:17:16 +0300

    modifications to controllers

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -2,7 +2,7 @@ const connection = require('../db/db.config');
 
 exports.fetchAllJobs = (req, res) => {
     try {
-        let query = "SELECT * FROM job_listings";
+        let query = "SELECT * FROM job_listings_det";
         connection.query(query, (error, response) => {
             if (!error) {
                 res.send(response)
@@ -15,10 +15,53 @@ exports.fetchAllJobs = (req, res) => {
 
 exports.fetchRemoteJobs = (req, res) => {
     try {
-        let query = `SELECT * FROM job_listings WHERE location = "remote"`;
+        let query = `SELECT * FROM job_listings_det WHERE location = "remote"`;
         connection.query(query, (error, response) => {
             if (!error) {
                 res.send(response)
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ msg: "Internal Server Error" })
+    }
+}
+
+exports.fetchFullTimeJobs = (req, res) => {
+    try {
+        let query = `SELECT * FROM job_listings_det WHERE meta = "Full-time"`;
+        connection.query(query, (error, response) => {
+            if (!error) {
+                res.send(response)
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ msg: "Internal Server Error" })
+    }
+}
+
+exports.fetchTodayJobs = (req, res) => {
+    try {
+        let query = `SELECT * FROM job_listings_det WHERE posted_at = "PostedToday"`;
+        connection.query(query, (error, response) => {
+            if (!error) {
+                res.send(response)
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ msg: "Internal Server Error" })
+    }
+}
+
+exports.fetchByCompanyName = (req, res) => {
+    try {
+        let query = `SELECT * FROM job_listings_det WHERE company LIKE ? `;
+        connection.query(query, ['%' + req.params.company_name + '%'], (error, response) => {
+            if (!error) {
+                if (response.length === 0) {
+                    res.json({ msg: "No Result For Search Parameters" })
+                }
+                res.send(response)
+                console.log(response);
             }
         });
     } catch (error) {
--- routes/jobListingRoute.js
+++ routes/jobListingRoute.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
 const {
-    fetchAllJobs, fetchRemoteJobs
+    fetchAllJobs, fetchRemoteJobs, fetchFullTimeJobs, fetchTodayJobs, fetchByCompanyName
 } = require('../controllers/job.controller');
 
 
 router.get('/', fetchAllJobs);
 router.get('/remote', fetchRemoteJobs);
+router.get('/full-time', fetchFullTimeJobs);
+router.get('/jobs-today', fetchTodayJobs);
+router.get('/:company_name', fetchByCompanyName);
 
 module.exports = router;
